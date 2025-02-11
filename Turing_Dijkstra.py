46c15f591e7335d9d407c6dbdce0198b96cc5d03
Victorferb <victor-ferreira12@hotmail.com>
2025-02-11 19:27:41 -0300

    Ideia do projeto

---
+++ Turing_Dijkstra.py
@@ -0,0 +1,13 @@
+"""
+Implementação de uma máquina de turing nao determinista e de fita limitada
+onde as arestas são ponderadas e dar como resultados:
+
+- fita final
+- caminho percorrido
+- soma ponderada das arestas
+
+O truque é alterar o grafo da máquina e trocar o bfs por um djkistra
+"""
+
+
+
