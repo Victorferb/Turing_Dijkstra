25f5bf7601d9a4b07c36c2de435f58ae6104e2fc
Victorferb <victor-ferreira12@hotmail.com>
2025-02-11 19:34:26 -0300

    função para executar a MT com Dijkstra

--- Turing_Dijkstra.py
+++ Turing_Dijkstra.py
@@ -30,3 +30,37 @@ class TuringMachine:
         priority_queue = [(0, self.start_state, self.head, list(self.tape), [])]  
         visited = set()
 
+        while priority_queue:
+            cost, state, head, tape, path = heapq.heappop(priority_queue)  
+            
+            
+            if (state, head, tuple(tape)) in visited:
+                continue
+            visited.add((state, head, tuple(tape)))
+            
+            
+            if state in self.accept_states:
+                return ''.join(tape), path, cost
+            
+            
+            current_symbol = tape[head] if 0 <= head < len(tape) else '_'
+            
+            
+            if (state, current_symbol) in self.transitions:
+                for next_state, write_symbol, move, weight in self.transitions[(state, current_symbol)]:
+                    new_tape = tape[:]
+                    new_tape[head] = write_symbol  
+                    new_head = head + (1 if move == 'R' else -1)  
+                    
+                    
+                    if new_head < 0:
+                        new_tape.insert(0, '_')
+                        new_head = 0
+                    elif new_head >= len(new_tape):
+                        new_tape.append('_')
+                    
+                    
+                    heapq.heappush(priority_queue, (cost + weight, next_state, new_head, new_tape, path + [(state, next_state, weight)]))
+        
+        return None  
+
